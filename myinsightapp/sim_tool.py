5181c7b379d805d1a8bd9ac222c24c49812a987b
Chevy Williams <ChevyWilliams@users.noreply.github.com>
2019-10-01 20:06:53 -0500

    Update app files

--- myinsightapp/sim_tool.py
+++ myinsightapp/sim_tool.py
@@ -17,69 +17,50 @@ import pickle
 
 
 # Load Data
-def load_data():
-    appdf_sub = pd.read_pickle('appdf_sub.pkl')
-    filename = 'GoogleNews-vectors-negative300.bin.gz'
+# def load_data():
+#     if selection == 'Art/Visual Art/Photography':
+#         df = pd.read_pickle('df_art.pkl')
+#     elif selection == 'Books/Zine/Print':
+#         df = pd.read_pickle('df_book.pkl')
+#     elif selection == 'Crafts/Textiles':
+#         df = pd.read_pickle('df_craft.pkl')
+#     elif selection == 'Design/Architecture':
+#         df = pd.read_pickle('df_design.pkl')
+#     elif selection == 'Equipment/Tools/DIY':
+#         df = pd.read_pickle('df_diy.pkl')
+#     elif selection == 'Fashion/Apparel':
+#         df = pd.read_pickle('df_fashion.pkl')
+#     elif selection == 'Food/Beverage':
+#         df = pd.read_pickle('df_food.pkl')
+#     elif selection == 'Games/Gaming':
+#         df = pd.read_pickle('df_games.pkl')
+#     elif selection == 'Media':
+#         df = pd.read_pickle('df_media.pkl')
+#     elif selection == 'Music':
+#         df = pd.read_pickle('df_music.pkl')
+#     elif selection == 'Performance Art':
+#         df = pd.read_pickle('df_perform.pkl')
+#     elif selection == 'Spaces/Places/Events':
+#         df = pd.read_pickle('df_events.pkl')
+#     elif selection == 'Tech/Web':
+#         df = pd.read_pickle('df_tech.pkl')
+#     elif selection == 'TV/Film/Webseries':
+#         df = pd.read_pickle('df_film.pkl')
+#     else:
+#         df = pd.read_pickle('df_other.pkl')
+#     return df
+
+def load_model():
+    filename = 'GoogleNews-vectors-negative300-SLIM.bin'
     emb_model = KeyedVectors.load_word2vec_format(filename, binary=True)
-    return appdf_sub, emb_model
-
-# Define a function that cleans strings: lowercase, remove select punctuation, tokenize
-def clean_text(my_str):
-    str_low = my_str.lower()
-    remove = string.punctuation
-    remove = remove.replace(".", " ").replace(",", " ").replace("'", " ")
-    pattern = r"[{}]".format(remove) # create the pattern
-    cleanstr = re.sub(pattern, " ", str_low)
-    finalword = word_tokenize(cleanstr)
-    return finalword
-
-
-
-# def ideavetter():
-#     if request.method == 'POST':
-#         for aKey in request.form:
-#             if aKey == 'pitch':
-#                 text = request.form[aKey]
-#         # text = request.form('pitch')
-#
-#         print(request.form['pitch'])
-#         print(text)
-#
-#         appdf_sub, emb_model = load_data()
-#     # accept user input
-#         # text = input("Pitch your idea here: ")
-#
-#     # clean and tokenize user input
-#         clean_input = clean_text(text)
-#
-#     # get vectors for input text
-# #     input_vecs = get_vectors(clean_input)
-#
-#     # calculate WMD distance
-#         input_list = []
-#         for i in appdf_sub['cleanblurb']:
-#             distance = emb_model.wmdistance(clean_input, i)
-#             input_list.append(distance)
-#
-#     # add input_veclist to df
-#         appdf_sub['distance'] = input_list
-#
-#     # sort dataframe in ascending order by distance
-#         appdf_sub.sort_values(by=['distance'])
-#
-#     # return url for top 3 rows
-#         for i in appdf_sub['state']:
-#             if i == 'successful':
-#                 return print(appdf_sub['projecturls'].head(3))
-    # return
-
+    return emb_model
 
 # Define a function that strips all punctuation and stop words
-# def stripper(my_str):
-#     str_low = my_str.lower()
-#     allpunc = string.punctuation
-#     pattern = r"[{}]".format(allpunc) # create the pattern
-#     cleanstr = re.sub(pattern, " ", str_low)
-#     tokenword = word_tokenize(cleanstr)
-#     cleanstr = [word for word in tokenword if word not in nltk.corpus.stopwords.words('english')]
-#     return cleanstr
+def stripper(my_str):
+    str_low = my_str.lower()
+    allpunc = string.punctuation
+    pattern = r"[{}]".format(allpunc) # create the pattern
+    cleanstr = re.sub(pattern, " ", str_low)
+    tokenword = word_tokenize(cleanstr)
+    cleanstr = [word for word in tokenword if word not in stopwords.words('english')]
+    return cleanstr
